3946f9a12bd457d48585d4256834ba42f620e77e
Emiliano <emilianoruben91@gmail.com>
2020-07-13 16:53:07 -0500

    se agrego ver perfil

---
+++ middlew/validoJWT.go
@@ -0,0 +1,18 @@
+package middlew
+
+import (
+	"net/http"
+	"github.com/emiliano080591/go-twittor/routers"
+)
+
+/*ValidoJWT permite validar el JWT que nos viene de la peticion*/
+func ValidoJWT(next http.HandlerFunc) http.HandlerFunc {
+	return func (w http.ResponseWriter,r *http.Request)  {
+		_,_,_,err:=routers.ProcesoToken(r.Header.Get("Authorization"))
+		if err != nil {
+			http.Error(w,"Error en el token "+err.Error(),http.StatusBadRequest)
+			return
+		}
+		next.ServeHTTP(w,r)
+	}
+}
