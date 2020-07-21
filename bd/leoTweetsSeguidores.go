eb3a0308d73dcb6155f665f4b05327fc2611a1c8
Emiliano <emilianoruben91@gmail.com>
2020-07-20 19:19:44 -0500

    Se acomodo leTweetsSeguidores de bd

--- bd/leoTweetsSeguidores.go
+++ bd/leoTweetsSeguidores.go
@@ -8,7 +8,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-/*LeoTweetsSeguidores lee los tweets de mis seguidores*/
+/*LeoTweetsSeguidores lee los tweets de mis seguidores */
 func LeoTweetsSeguidores(ID string, pagina int) ([]models.DevuelvoTweetsSeguidores, bool) {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
@@ -23,11 +23,10 @@ func LeoTweetsSeguidores(ID string, pagina int) ([]models.DevuelvoTweetsSeguidor
 	condiciones = append(condiciones, bson.M{
 		"$lookup": bson.M{
 			"from":         "tweet",
-			"localfield":   "usuariorelacionid",
+			"localField":   "usuariorelacionid",
 			"foreignField": "userid",
 			"as":           "tweet",
 		}})
-
 	condiciones = append(condiciones, bson.M{"$unwind": "$tweet"})
 	condiciones = append(condiciones, bson.M{"$sort": bson.M{"tweet.fecha": -1}})
 	condiciones = append(condiciones, bson.M{"$skip": skip})
@@ -39,6 +38,5 @@ func LeoTweetsSeguidores(ID string, pagina int) ([]models.DevuelvoTweetsSeguidor
 	if err != nil {
 		return result, false
 	}
-
 	return result, true
 }
